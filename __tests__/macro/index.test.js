c579e2fc6184f46cd449f3b90edc6246ed2f98fb
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-11-29 15:39:25 +0700

    Update macro tests

--- __tests__/macro/index.test.js
+++ __tests__/macro/index.test.js
@@ -3,13 +3,19 @@ import plugin from 'babel-plugin-macros';
 
 pluginTester({
   plugin,
+  title: 'laz macros',
   snapshot: true,
   babelOptions: { filename: __filename },
-  tests: [
-    `
+  tests: {
+    'Thunk > any expression > wrap in a thunk': `
       import { thunk } from '../../src/macro';
 
-      thunk(1);
+      const lazy1 = thunk(1);
     `,
-  ],
+    'Stream > array literal > transform to stream structure': `
+      import { stream } from '../../src/macro';
+
+      const stream1 = stream([1, 2, 3]);
+    `,
+  },
 });
